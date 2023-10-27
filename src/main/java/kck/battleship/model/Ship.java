a4e3278c2e13ac57a0b0fe444beccdc4155a9f3a
mat3usq <mmogielnicki123@gmail.com>
2023-10-27 20:53:26 +0200

    Ship class

---
+++ src/main/java/kck/battleship/model/Ship.java
@@ -0,0 +1,39 @@
+package kck.battleship.model;
+
+public class Ship {
+    private final String name;
+    private final int length;
+    private Position position;
+    private Direction direction;
+
+    public Ship(String name, int length){
+        this.name = name;
+        this.length = length;
+        this.position = null;
+        this.direction = null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+}
