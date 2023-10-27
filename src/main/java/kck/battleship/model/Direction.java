8d6777010f305554ffe464fc8487fe8430e2b2ed
mat3usq <mmogielnicki123@gmail.com>
2023-10-27 20:42:50 +0200

    Direction class

---
+++ src/main/java/kck/battleship/model/Direction.java
@@ -0,0 +1,20 @@
+package kck.battleship.model;
+
+import kck.battleship.exceptions.DirectionException;
+
+public enum Direction {
+    HORIZONTAL,
+    VERTICAL;
+
+    public static Direction decode(char _char) throws DirectionException {
+        if (_char == 'h' || _char == 'H') return HORIZONTAL;
+        else if (_char == 'v' || _char == 'V') return VERTICAL;
+        else throw new DirectionException("Podany znak '" + _char + "' nie jest poprawny");
+    }
+
+    public static Direction decode(String string) throws DirectionException {
+        if (string.toLowerCase().equals("horizontal")) return HORIZONTAL;
+        else if (string.toLowerCase().equals("vertical")) return VERTICAL;
+        else throw new DirectionException("Podany string '" + string + "' nie jest poprawny");
+    }
+}
