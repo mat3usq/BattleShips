f8df2b93e98d184773fb2eb174f46a5e74730e07
mat3usq <mmogielnicki123@gmail.com>
2023-10-27 21:22:01 +0200

    Types of Ships enum

---
+++ src/main/java/kck/battleship/model/ShipT.java
@@ -0,0 +1,36 @@
+package kck.battleship.model;
+
+public enum ShipT {
+    SUBMARINE(3, 2),
+    CRUISER(2, 3),
+    DESTROYER(2, 4),
+    BATTLESHIP(1, 5),
+    CARRIER(1, 5);
+
+
+    private final int numberOfShips;
+    private final int shipLength;
+
+    ShipT (int numShips, int shipLength) {
+        this.numberOfShips = numShips;
+        this.shipLength = shipLength;
+    }
+
+    public int getShipLength() {
+        return shipLength;
+    }
+    public int getNumShips() {
+        return numberOfShips;
+    }
+
+    public static int lengthAllShips(){
+        int sum = 0;
+        for (ShipT type: ShipT.values()) sum += type.shipLength * type.numberOfShips;
+        return sum;
+    }
+    public static int sizeAllShips(){
+        int sum = 0;
+        for (ShipT type: ShipT.values()) sum += type.numberOfShips;
+        return sum;
+    }
+}
