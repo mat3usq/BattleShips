11d0a7ace1efec8172362d40c1de7898b0a735c7
mat3usq <mmogielnicki123@gmail.com>
2023-11-09 02:08:14 +0100

    font size

--- src/main/java/kck/battleship/BattleShips.java
+++ src/main/java/kck/battleship/BattleShips.java
@@ -5,14 +5,21 @@ import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 
 import com.googlecode.lanterna.terminal.Terminal;
 import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration;
 import kck.battleship.controller.GameException;
 import kck.battleship.view.TextView;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class BattleShips {
     public static void main(String[] args) throws IOException, GameException, InterruptedException {
-        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+        Font myFont = new Font("Monospaced", Font.PLAIN, 27);
+        AWTTerminalFontConfiguration myFontConfiguration = AWTTerminalFontConfiguration.newInstance(myFont);
+        DefaultTerminalFactory dtf = new DefaultTerminalFactory();
+        dtf.setForceAWTOverSwing(true);
+        dtf.setTerminalEmulatorFontConfiguration(myFontConfiguration);
+        Terminal terminal = dtf.createTerminal();
         Screen screen = new TerminalScreen(terminal);
         screen.startScreen();
         screen.setCursorPosition(null);
